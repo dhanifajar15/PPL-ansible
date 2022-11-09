2057803a1b16f66d32e39ecd3931da2f691b4382
dhanifajar15 <dhanifajar15@gmail.com>
2022-11-09 00:14:51 +0000

    fix

--- files/templates/backend/script-deploy-backend.sh
+++ files/templates/backend/script-deploy-backend.sh
@@ -2,15 +2,15 @@
 #----------------------------------------
 
 PATH='/home/dhani-azure/script-deploy/frontend'
-DOCKER_USERNAME='dhanifajar15'
-DOCKER_PASSWORD='XXXXXXXXXXXX'
+# DOCKER_USERNAME='XXXXXXXXXXXX'
+# DOCKER_PASSWORD='XXXXXXXXXXXX'
 COMPOSE_NAME='backend'
 #----------------------------------------
 
 echo "Deploy new image"
 
 cd $PATH &&
-docker login --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD &&
+# docker login --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD &&
 docker-compose -f $COMPOSE_NAME.yml pull &&
 docker-compose -f $COMPOSE_NAME.yml up -d
 
